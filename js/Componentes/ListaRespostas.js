9ff0555a0a39e3a240b613c3c143f2e0fb80de63
Paulo <pauloh16.pdr@gmail.com>
2021-02-18 04:49:06 -0300

    Adicionar dados dinamicos via fetch api

--- js/Componentes/ListaRespostas.js
+++ js/Componentes/ListaRespostas.js
@@ -2,12 +2,30 @@ import CardResposta from './CardResposta.js';
 
 export default {
     name: 'ListaRespostas',
+    data: () => ({
+        respostas: null
+    }),
     components: {
         CardResposta
     },
+    methods: {
+        async buscarRespostas() {
+            const res = await fetch("../../db/respostas.json");
+            this.respostas = await res.json();
+        }
+    },
     template: (
-        `<ul class="lista_respostas">
-            <card-resposta numero="1" justificativa="Pois não existe vegetal melhor do que batata" resposta="batata"></card-resposta>
+        `<ul class="lista_respostas" v-if="respostas">
+            <card-resposta
+                v-for="({ numero, resposta, justificativa }) in respostas"
+                key="numero"
+                :numero="numero"
+                :justificativa="justificativa"
+                :resposta="resposta"
+            ></card-resposta>
         </ul>`
-    )
+    ),
+    created() {
+        this.buscarRespostas();
+    }
 };
